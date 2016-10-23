518fdecce68654a9064d1b2eae1620293edd5145
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 20:26:20 +0200

    exercice 4 and changed file

--- Exercice4/ex4.sh
+++ Exercice4/ex4.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-FILE="copie"
+FILE="calendar"
 
 if [ -e "$FILE" ]; then
 	rm $FILE
 fi
 
 cp myical_promo_gmt.2015.09.10.ics $FILE
-sed -i "s/BEGIN:VEVENT/<VEVENT>/g" $FILE
-sed -i "s/END:VEVENT/<\/VEVENT>/g" $FILE
+sed -i "s/BEGIN:\(.*\)/<\1>/g" $FILE
+sed -i "s/END:\(.*\)/<\/\1>/g" $FILE
 
 sed -i "s/\([A-Z;=]*\):\(.*\)/<\1>\2<\/\1>/g" $FILE
