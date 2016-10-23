f02e26798415346cca1810723dc4b144b7d0029c
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 23:28:17 +0200

    final script and mowi files

--- Exercice5/2014-DN-TP1-DATA/ex5.sh
+++ Exercice5/2014-DN-TP1-DATA/ex5.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-rm tmp
-sed "s/\(.*\)\t\(.*\)\t\(.*\)/Majeure\t\3\t\2\t\L\2\.\L\3@gmail\.myst/g" maj.txt >> tmp
 sed "s/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/MWI\t\U\2\E\t\3\t\4/g" mwi.txt >> tmp
 sed "s/\(.*\)\t\(.*\)\t\(.*\)/Option\t\1\t\2\t\3/g" opt.txt >> tmp
+sed "s/\(.*\)\t\(.*\)\t\(.*\)/Majeure\t\3\t\2\t\L\2\.\L\3@gmail\.myst/g" maj.txt >> tmp
+
+sort -k2 tmp > mowi-nom.txt
+sort -k1 -k2 tmp > mowi-prov-nom.txt
+
+rm tmp
