0409f811ee41557bbff58c3f9b37acc7617ea787
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 17:24:09 +0200

    exercice 3.6

--- Exercice3/ex3-5.sh
+++ Exercice3/ex3-5.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
 declare -a sulfixes
+declare -i count
 #sulfixes = file extensions
 sulfixes=$(ls coreutils-8.25/src| cut -s -d "." -f2 | sort -u)
 for s in $sulfixes; do
 	echo "SULFIXE: $s"
-	ls coreutils-8.25/src| grep "\.$s$"
+	files=$(ls coreutils-8.25/src| grep "\.$s$")
 	echo "TOTAL: $(ls coreutils-8.25/src| grep "\.$s$" | wc -l)"
+	count=0
+	for f in $files; do	
+		echo $f
+		count=$count+$(cat coreutils-8.25/src/$f | wc -l)
+	done
+	echo "TOTAL LINES: $count"
 done
 	
