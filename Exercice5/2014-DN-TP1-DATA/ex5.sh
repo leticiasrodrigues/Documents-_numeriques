89c6148df2224001909854e9fd89eea1ef4ea5a4
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 23:22:49 +0200

    fichier opt

--- Exercice5/2014-DN-TP1-DATA/ex5.sh
+++ Exercice5/2014-DN-TP1-DATA/ex5.sh
@@ -3,3 +3,4 @@
 rm tmp
 sed "s/\(.*\)\t\(.*\)\t\(.*\)/Majeure\t\3\t\2\t\L\2\.\L\3@gmail\.myst/g" maj.txt >> tmp
 sed "s/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/MWI\t\U\2\E\t\3\t\4/g" mwi.txt >> tmp
+sed "s/\(.*\)\t\(.*\)\t\(.*\)/Option\t\1\t\2\t\3/g" opt.txt >> tmp
