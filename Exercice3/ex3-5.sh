d7db3da4dcca472732ba726296608c879f1228dc
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 18:10:25 +0200

    exercice 3.7

--- Exercice3/ex3-5.sh
+++ Exercice3/ex3-5.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 
-declare -a sulfixes
-declare -i count
-#sulfixes = file extensions
-sulfixes=$(ls coreutils-8.25/src| cut -s -d "." -f2 | sort -u)
-for s in $sulfixes; do
-	echo "SULFIXE: $s"
-	files=$(ls coreutils-8.25/src| grep "\.$s$")
-	echo "TOTAL: $(ls coreutils-8.25/src| grep "\.$s$" | wc -l)"
-	count=0
-	for f in $files; do	
-		echo $f
-		count=$count+$(cat coreutils-8.25/src/$f | wc -l)
+function ex(){
+	sulfixes=$(ls $1 | cut -s -d "." -f2 | sort -u)
+	for s in $sulfixes; do
+		echo "SULFIXE: $s"
+		files=$(ls $1| grep "\.$s$")
+		echo "TOTAL: $(ls $1 | grep "\.$s$" | wc -l)"
+		count=0
+		for f in $files; do	
+			count=$(($count+$(cat $1/$f | wc -l)))
+		done
+		echo "TOTAL LINES: $count"
 	done
-	echo "TOTAL LINES: $count"
+}
+directories=$(ls -d coreutils-8.25/*/)
+for d in $directories; do
+	echo $d
+	ex $d
 done
-	
