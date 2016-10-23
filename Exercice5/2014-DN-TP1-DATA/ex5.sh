e2c50633a26e7cf45f69715ac8802e0613f35114
leticiasrodrigues <leticiasilva_650@hotmail.com>
2016-10-23 23:20:32 +0200

    fichiers mwi et maj

---
+++ Exercice5/2014-DN-TP1-DATA/ex5.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+rm tmp
+sed "s/\(.*\)\t\(.*\)\t\(.*\)/Majeure\t\3\t\2\t\L\2\.\L\3@gmail\.myst/g" maj.txt >> tmp
+sed "s/\(.*\)\t\(.*\)\t\(.*\)\t\(.*\)/MWI\t\U\2\E\t\3\t\4/g" mwi.txt >> tmp
